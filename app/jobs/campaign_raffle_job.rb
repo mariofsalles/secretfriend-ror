c59977a2d2191cfe3a4ec6ac175b96a3626b6d98
Mario Salles <mariofsalles@gmail.com>
2019-03-26 20:04:07 +0000

    Create campaign_raffle_job

---
+++ app/jobs/campaign_raffle_job.rb
@@ -0,0 +1,20 @@
+class CampaignRaffleJob < ApplicationJob
+  queue_as :default
+
+  def perform(campaign)
+    results = RaffleService.new(campaign).call
+
+    campaign.members.each do |member| 
+      member.set_pixel
+    end
+
+    results.each do |result|
+      CampaignMailer.raffle(campaign, result.first, result.last).delivery_now
+    end
+    campaign.update(status: :finished)
+
+    # if results == false
+    #   Send mail to owner of campaign(desafio)
+    # end
+  end
+end
